5b52ceb3c5da104281df5e66942a1cd1fbc3dadb
Cody Switzer <cswitzer85@gmail.com>
2020-02-13 21:37:21 -0600

    added second prompt and changed API URL

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const util = require("util");
 
-inquirer
-	.prompt({
-		message: "Enter your GitHub user name",
-		name: "username"
-	})
-	.then(function ({ username}) {
-		const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter your GitHub user name",
+      name: "userName"
+  },
+    {
+      type: "input",
+      message: "What is the name of your repository?",
+      name: "repoName"
+  },])
+	}
+	.then(function ({ userName}) {
+		const queryUrl = `https://api.github.com/users/${userName}`;
 		axios.get(queryUrl).then(function (res) {
-			const repoNames = res.data.map(function (repo) {
-				return repo.name;//Grabs all repo names, how to grab info for certain repo??????????
+			const userEmail = (function (users.email) {
+				console.log(userEmail);
 			});
 
-      const repoDataThatIDontEvenKnowINeedYet = repoNames.join("\n");
+    //   const repoDataThatIDontEvenKnowINeedYet = repoNames.join("\n");
       // const description;
       // const tableOfContents;
       // const installation;
