aa5ef4135defd1237015aeb17072672df7ccab8e
Cody Switzer <cswitzer85@gmail.com>
2020-02-14 21:58:28 -0600

    updated api & generateMarkdown js files.

--- api.js
+++ api.js
@@ -1,7 +1,14 @@
-const api = {
-  getUser(username) {
+const axios = require("axios");
 
-  }
-};
+axios
+	.get("https://api.github.com/users/${userName}")
+	.then(function (res) {
+		console.log(res.data);
+		// const queryUrl = `https://api.github.com/users/${userName}`;
+    // const api = {res.data.avatar_url};
+    
+		// email data found using "eventsPageAPI[0].payload.commits[0].author.email"
+		// user avatar URL found using "userPageAPI.avatar_url"
+	});
 
 module.exports = api;
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,27 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+## ${data.title}
+[![npm version](https://badge.fury.io/js/inquirer.svg)](https://badge.fury.io/js/inquirer)//Badge markdown
+## ${data.description}
+## ${tableOfContents}
+  * Title
+  * Description
+  * Table of Contents
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+## ${installation}
+## ${usage}
+## ${license}
+## ${contributing}
+## ${tests}
+## ${questions}
 
 `;
-}
+};
+
 
 module.exports = generateMarkdown;
